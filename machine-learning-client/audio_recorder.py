8fb26b65193a7ddd7ddb9d1242a8da4a418fda5d
YifanZZZZZZ <182949398+YifanZZZZZZ@users.noreply.github.com>
2025-04-11 00:16:41 -0400

    Fix Black error v1

--- machine-learning-client/audio_recorder.py
+++ machine-learning-client/audio_recorder.py
@@ -1,11 +1,15 @@
 """Handles audio recording."""
+
 import sounddevice as sd
 from scipy.io.wavfile import write
+
 
 def record_audio(filename="audio.wav", duration=5, samplerate=16000):
     """Record audio and save it."""
     print(f"Recording for {duration} seconds...")
-    audio = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='int16')
+    audio = sd.rec(
+        int(duration * samplerate), samplerate=samplerate, channels=1, dtype="int16"
+    )
     sd.wait()
     write(filename, samplerate, audio)
     print(f"Audio saved to {filename}")
--- machine-learning-client/emotion_analyzer.py
+++ machine-learning-client/emotion_analyzer.py
@@ -1,23 +1,24 @@
 """Analyze audio emotion by using pre-trained model"""
+
 import torch
 import torch.nn.functional as F
 import torchaudio
 
 from speechbrain.inference import EncoderClassifier
 
+
 def analyze_emotion(file_path):
     """Apply the third party pre-trained model to analyze the audio."""
     print("Loading model...")
     classifier = EncoderClassifier.from_hparams(
         source="speechbrain/emotion-recognition-wav2vec2-IEMOCAP",
-        savedir="pretrained_models/emotion-recognition"
+        savedir="pretrained_models/emotion-recognition",
     )
 
     classifier.hparams.label_encoder.expect_len(4)
     print("Loading audio...")
     waveform, _ = torchaudio.load(file_path)
     print("Extracting features with wav2vec2...")
-
 
     with torch.no_grad():
         wav2vec_out = classifier.mods.wav2vec2(waveform)
